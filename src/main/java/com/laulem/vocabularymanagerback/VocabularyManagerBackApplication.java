951ba1c79e52b74b9eb32990f53cdcde99257d09
Alexandre Lemaire <alexandre.lemaire@laulem.com>
2022-07-10 11:00:31 +0200

    Architecture de base

---
+++ src/main/java/com/laulem/vocabularymanagerback/VocabularyManagerBackApplication.java
@@ -0,0 +1,13 @@
+package com.laulem.vocabularymanagerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabularyManagerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabularyManagerBackApplication.class, args);
+	}
+
+}
